c035ee384ed1d35b94cf3e81e4dfddcd38f822a1
fateh-webpro <162103535+fateh-webpro@users.noreply.github.com>
2026-07-23 01:40:39 +0300

    Remove unused webhook log create action

--- app/Filament/Resources/WebhookLogs/Pages/ListWebhookLogs.php
+++ app/Filament/Resources/WebhookLogs/Pages/ListWebhookLogs.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\WebhookLogs\Pages;
 
 use App\Filament\Resources\WebhookLogs\WebhookLogResource;
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListWebhookLogs extends ListRecords
@@ -12,8 +11,6 @@ class ListWebhookLogs extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [
-            CreateAction::make(),
-        ];
+        return [];
     }
 }
